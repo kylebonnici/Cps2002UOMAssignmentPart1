b7e02aae51282f73ee7ea0394e457b2d215ed830
kylebonnici <kylebonnici@hotmail.com>
2015-03-31 16:45:30 +0200

    Small Change + updates .ignoregit to ignore workspace.xml :)

--- cps2002AssignmentPart1/src/test/java/TestApp.java
+++ cps2002AssignmentPart1/src/test/java/TestApp.java
@@ -3,6 +3,6 @@
  */
 public class TestApp {
     public static void main (String args[]){
-        System.out.println(":) --> MT 03 !!");
+        System.out.println(":) --> MT 03 !! :D");
     }
 }
