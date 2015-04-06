e01798ac43f6ad1545246514fd8525d7c481c9bb
mark92m <mark92m@gmail.com>
2015-04-06 11:29:44 +0200

    Conflict Resolve.

--- src/main/java/AccountDatabase.java
+++ src/main/java/AccountDatabase.java
@@ -5,14 +5,19 @@ import java.util.*;
 
 public class AccountDatabase {
 
+    private ArrayList<Account> database;
 
     public AccountDatabase(){
         ArrayList<Account> database = new ArrayList<Account>();
     }
+
     public Account getAccount(int AccountNumber){
         Account acc = new Account(0);
 
         return acc;
+    }
+    public int getSize(){
+        return 0;
     }
 
     public void createNewAccount( int accountNumber){
