3c8d43ed9166e185906febe1aaa44ffde0a9bcb0
mark92m <mark92m@gmail.com>
2015-04-06 11:18:23 +0200

    Database Array initialiser.

--- src/main/java/AccountDatabase.java
+++ src/main/java/AccountDatabase.java
@@ -1,12 +1,18 @@
 /**
  * Created by Mark on 01/04/15.
  */
+import java.util.*;
+
 public class AccountDatabase {
 
+    private ArrayList<Account> database;
     public int getSize(){
         return 1;
     }
 
+    public AccountDatabase(){
+        ArrayList<Account> database = new ArrayList<Account>();
+    }
     public Account getAccount(int AccountNumber){
         Account acc = new Account();
 
@@ -14,7 +20,7 @@ public class AccountDatabase {
     }
 
     public void createNewAccount( int accountNumber){
-        
+
     }
 
 }
