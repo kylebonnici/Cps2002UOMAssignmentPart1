e28305c71d3a0c504c6c08333f2fc99dfcd740af
mark92m <mark92m@gmail.com>
2015-04-06 09:13:03 +0200

    Committing before Pull.

--- cps2002AssignmentPart1/src/main/java/Transaction.java
+++ cps2002AssignmentPart1/src/main/java/Transaction.java
@@ -2,4 +2,12 @@
  * Created by Mark on 01/04/15.
  */
 public class Transaction {
+
+    public int sourceAccountNumber;
+    public int destinationAccountNumber;
+    public long amount;
+
+    public boolean process(){
+        return true;
+    }
 }
