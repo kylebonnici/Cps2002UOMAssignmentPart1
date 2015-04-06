1ed2511c66882b74b09d4489df35244f6b3a29f0
kylebonnici <kylebonnici@hotmail.com>
2015-04-06 14:29:50 +0200

    Added 15 sec constrain on transection

--- src/main/java/um/edu/mt/cps2002/assignment/part1/Transaction.java
+++ src/main/java/um/edu/mt/cps2002/assignment/part1/Transaction.java
@@ -1,5 +1,8 @@
 package um.edu.mt.cps2002.assignment.part1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Mark on 01/04/15.
  */
@@ -8,25 +11,42 @@ public class Transaction extends AccountDatabase {
     public int sourceAccountNumber;
     public int destinationAccountNumber;
     public long amount;
+    private Map<Integer,Long> lastTransectionTime;
+
+    public Transaction(){
+        lastTransectionTime = new HashMap<Integer,Long>();
+    }
 
     public boolean process(){
         Account accSrc = this.getAccount(sourceAccountNumber);
         Account accDst = this.getAccount(destinationAccountNumber);
 
+        Long srcTime = lastTransectionTime.get(new Integer(accSrc.getAccountNumber()));
+        Long dstTime = lastTransectionTime.get(new Integer(accDst.getAccountNumber()));
 
-        if (accSrc != null && accDst != null){
-           if (accSrc.adjustBalance(-amount)){
-               if (accDst.adjustBalance(amount)){
-                   return true;
-               }else{
-                   accSrc.adjustBalance(amount);
-                   return false;
-               }
-           }else {
-               return false;
-           }
-        }else {
+        if (srcTime != null && srcTime.longValue() + 15000  > System.currentTimeMillis()){
             return false;
+        }else if (dstTime != null && dstTime.longValue() + 15000  > System.currentTimeMillis()){
+            return false;
+        }else {
+            if (accSrc != null && accDst != null) {
+                if (accSrc.adjustBalance(-amount)) {
+                    if (accDst.adjustBalance(amount)) {
+
+                        lastTransectionTime.put(new Integer(accSrc.getAccountNumber()), new Long(System.currentTimeMillis()));
+                        lastTransectionTime.put(new Integer(accDst.getAccountNumber()), new Long(System.currentTimeMillis()));
+
+                        return true;
+                    } else {
+                        accSrc.adjustBalance(amount);
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
         }
     }
 }
