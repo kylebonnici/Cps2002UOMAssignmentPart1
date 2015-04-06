3217f3d2b069bcd3dbb83d9f4473f6c5711da359
kylebonnici <kylebonnici@hotmail.com>
2015-04-06 09:26:13 +0200

    Test again

--- cps2002AssignmentPart1/src/main/java/TestClass.java
+++ cps2002AssignmentPart1/src/main/java/TestClass.java
@@ -2,4 +2,11 @@
  * Created by kylebonnici on 31/03/15.
  */
 public class TestClass {
+    public String sayHello(){
+        return "";
+    }
+
+    public String sayBye(){
+        return "";
+    }
 }
--- cps2002AssignmentPart1/src/test/java/TestApp.java
+++ cps2002AssignmentPart1/src/test/java/TestApp.java
@@ -1,9 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by kylebonnici on 31/03/15.
  */
+
+
 public class TestApp {
-    public static void main (String args[]){
-        System.out.println(":) --> MT 03 !! :D");
-        System.out.println(" Virago is nice too  :) ");
+    TestClass c;
+
+    @Before
+    public void reGenerateCalcInstance(){
+        c = new TestClass();
     }
+
+    @Test
+    public void testAdd(){
+        String ans = c.sayHello();
+        assertEquals("Hello",ans);
+    }
+
+    @Test
+    public void testSubtract(){
+        String ans = c.sayBye();
+        assertEquals("Bye",ans);
+    }
+
 }
