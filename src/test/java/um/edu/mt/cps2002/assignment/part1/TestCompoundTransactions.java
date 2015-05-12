f350f341e5ea917988a49373e9f10d1407432713
kylebonnici <kylebonnici@hotmail.com>
2015-05-12 16:03:13 +0200

    Updated Test to cover time (15 sec) between transections for components

--- src/test/java/um/edu/mt/cps2002/assignment/part1/TestCompoundTransactions.java
+++ src/test/java/um/edu/mt/cps2002/assignment/part1/TestCompoundTransactions.java
@@ -178,6 +178,144 @@ public class TestCompoundTransactions {
     }
 
 
+    @Test
+    public void testValidCompoundTransactionTwiceTimeError1(){
+        int amount = 200;
+        int startAmount = 1000;
+
+        CompoundTransaction ctBuyProperty = new CompoundTransaction();
+        CompoundTransaction ctPrepareLoan = new CompoundTransaction();
+        CompoundTransaction ctPayFees = new CompoundTransaction();
+
+        ctBuyProperty.addChildTransaction(ctPrepareLoan);
+        ctBuyProperty.addChildTransaction(ctPayFees);
+
+        Transaction trAllLoanFunds = new Transaction(tran.getAccountDatabase().getAccount(1234),tran.getAccountDatabase().getAccount(4567),amount);
+        Transaction trTranToSeller = new Transaction(tran.getAccountDatabase().getAccount(4567),tran.getAccountDatabase().getAccount(6781),amount);
+
+        ctPrepareLoan.addChildTransaction(trAllLoanFunds);
+        ctPrepareLoan.addChildTransaction(trTranToSeller);
+
+        Transaction trPayLegalFees = new Transaction(tran.getAccountDatabase().getAccount(1111),tran.getAccountDatabase().getAccount(2321),(long)(amount*0.18));
+        Transaction trPayTaxes = new Transaction(tran.getAccountDatabase().getAccount(1111),tran.getAccountDatabase().getAccount(2212),(long)(amount*0.18));
+
+        ctPayFees.addChildTransaction(trPayLegalFees);
+        ctPayFees.addChildTransaction(trPayTaxes);
+
+
+        Assert.assertEquals(true, tran.processTransaction(ctBuyProperty));
+
+        Assert.assertEquals(startAmount - amount, tran.getAccountDatabase().getAccount(1234).getAccountBalance());
+        Assert.assertEquals(startAmount + amount, tran.getAccountDatabase().getAccount(6781).getAccountBalance());
+        Assert.assertEquals(startAmount, tran.getAccountDatabase().getAccount(4567).getAccountBalance());
+
+        Assert.assertEquals(startAmount+(long)(amount*0.18), tran.getAccountDatabase().getAccount(2321).getAccountBalance());
+        Assert.assertEquals(startAmount - (2 * (long) (amount * 0.18)), tran.getAccountDatabase().getAccount(1111).getAccountBalance());
+        Assert.assertEquals(startAmount + (long) (amount * 0.18), tran.getAccountDatabase().getAccount(2212).getAccountBalance());
+
+
+        ctBuyProperty = new CompoundTransaction();
+        ctPrepareLoan = new CompoundTransaction();
+        ctPayFees = new CompoundTransaction();
+
+        ctBuyProperty.addChildTransaction(ctPrepareLoan);
+        ctBuyProperty.addChildTransaction(ctPayFees);
+
+        trAllLoanFunds = new Transaction(tran.getAccountDatabase().getAccount(1234),tran.getAccountDatabase().getAccount(4567),amount);
+        trTranToSeller = new Transaction(tran.getAccountDatabase().getAccount(4567),tran.getAccountDatabase().getAccount(6781),amount);
+
+        ctPrepareLoan.addChildTransaction(trAllLoanFunds);
+        ctPrepareLoan.addChildTransaction(trTranToSeller);
+
+        trPayLegalFees = new Transaction(tran.getAccountDatabase().getAccount(1111),tran.getAccountDatabase().getAccount(2321),(long)(amount*0.18));
+        trPayTaxes = new Transaction(tran.getAccountDatabase().getAccount(1111),tran.getAccountDatabase().getAccount(2212),(long)(amount*0.18));
+
+        ctPayFees.addChildTransaction(trPayLegalFees);
+        ctPayFees.addChildTransaction(trPayTaxes);
+
+        Assert.assertEquals(false, tran.processTransaction(ctBuyProperty));
+
+        Assert.assertEquals(startAmount - amount, tran.getAccountDatabase().getAccount(1234).getAccountBalance());
+        Assert.assertEquals(startAmount + amount, tran.getAccountDatabase().getAccount(6781).getAccountBalance());
+        Assert.assertEquals(startAmount, tran.getAccountDatabase().getAccount(4567).getAccountBalance());
+
+        Assert.assertEquals(startAmount+(long)(amount*0.18), tran.getAccountDatabase().getAccount(2321).getAccountBalance());
+        Assert.assertEquals(startAmount - (2 * (long) (amount * 0.18)), tran.getAccountDatabase().getAccount(1111).getAccountBalance());
+        Assert.assertEquals(startAmount + (long) (amount * 0.18), tran.getAccountDatabase().getAccount(2212).getAccountBalance());
+
+
+    }
+
+    @Test
+    public void testValidCompoundTransactionTwiceWait15Sec1(){
+        int amount = 200;
+        int startAmount = 1000;
+
+        CompoundTransaction ctBuyProperty = new CompoundTransaction();
+        CompoundTransaction ctPrepareLoan = new CompoundTransaction();
+        CompoundTransaction ctPayFees = new CompoundTransaction();
+
+        ctBuyProperty.addChildTransaction(ctPrepareLoan);
+        ctBuyProperty.addChildTransaction(ctPayFees);
+
+        Transaction trAllLoanFunds = new Transaction(tran.getAccountDatabase().getAccount(1234),tran.getAccountDatabase().getAccount(4567),amount);
+        Transaction trTranToSeller = new Transaction(tran.getAccountDatabase().getAccount(4567),tran.getAccountDatabase().getAccount(6781),amount);
+
+        ctPrepareLoan.addChildTransaction(trAllLoanFunds);
+        ctPrepareLoan.addChildTransaction(trTranToSeller);
+
+        Transaction trPayLegalFees = new Transaction(tran.getAccountDatabase().getAccount(1111),tran.getAccountDatabase().getAccount(2321),(long)(amount*0.18));
+        Transaction trPayTaxes = new Transaction(tran.getAccountDatabase().getAccount(1111),tran.getAccountDatabase().getAccount(2212),(long)(amount*0.18));
+
+        ctPayFees.addChildTransaction(trPayLegalFees);
+        ctPayFees.addChildTransaction(trPayTaxes);
+
+
+        Assert.assertEquals(true, tran.processTransaction(ctBuyProperty));
+
+        Assert.assertEquals(startAmount - amount, tran.getAccountDatabase().getAccount(1234).getAccountBalance());
+        Assert.assertEquals(startAmount + amount, tran.getAccountDatabase().getAccount(6781).getAccountBalance());
+        Assert.assertEquals(startAmount, tran.getAccountDatabase().getAccount(4567).getAccountBalance());
+
+        Assert.assertEquals(startAmount+(long)(amount*0.18), tran.getAccountDatabase().getAccount(2321).getAccountBalance());
+        Assert.assertEquals(startAmount - (2 * (long) (amount * 0.18)), tran.getAccountDatabase().getAccount(1111).getAccountBalance());
+        Assert.assertEquals(startAmount + (long) (amount * 0.18), tran.getAccountDatabase().getAccount(2212).getAccountBalance());
+
+
+        wait15Sec();
+
+        ctBuyProperty = new CompoundTransaction();
+        ctPrepareLoan = new CompoundTransaction();
+        ctPayFees = new CompoundTransaction();
+
+        ctBuyProperty.addChildTransaction(ctPrepareLoan);
+        ctBuyProperty.addChildTransaction(ctPayFees);
+
+        trAllLoanFunds = new Transaction(tran.getAccountDatabase().getAccount(1234),tran.getAccountDatabase().getAccount(4567),amount);
+        trTranToSeller = new Transaction(tran.getAccountDatabase().getAccount(4567),tran.getAccountDatabase().getAccount(6781),amount);
+
+        ctPrepareLoan.addChildTransaction(trAllLoanFunds);
+        ctPrepareLoan.addChildTransaction(trTranToSeller);
+
+        trPayLegalFees = new Transaction(tran.getAccountDatabase().getAccount(1111),tran.getAccountDatabase().getAccount(2321),(long)(amount*0.18));
+        trPayTaxes = new Transaction(tran.getAccountDatabase().getAccount(1111),tran.getAccountDatabase().getAccount(2212),(long)(amount*0.18));
+
+        ctPayFees.addChildTransaction(trPayLegalFees);
+        ctPayFees.addChildTransaction(trPayTaxes);
+
+        Assert.assertEquals(true, tran.processTransaction(ctBuyProperty));
+
+        Assert.assertEquals(startAmount - (2*amount), tran.getAccountDatabase().getAccount(1234).getAccountBalance());
+        Assert.assertEquals(startAmount + (2*amount), tran.getAccountDatabase().getAccount(6781).getAccountBalance());
+        Assert.assertEquals(startAmount, tran.getAccountDatabase().getAccount(4567).getAccountBalance());
+
+        Assert.assertEquals(startAmount+(long)((2*amount)*0.18), tran.getAccountDatabase().getAccount(2321).getAccountBalance());
+        Assert.assertEquals(startAmount - (2 * (long) ((2*amount) * 0.18)), tran.getAccountDatabase().getAccount(1111).getAccountBalance());
+        Assert.assertEquals(startAmount + (long) ((2*amount) * 0.18), tran.getAccountDatabase().getAccount(2212).getAccountBalance());
+
+
+    }
+
 
 }
 
