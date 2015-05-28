1ea8979dd33ea0cb602eef3770baf740f3771aa6
kylebonnici <kylebonnici@hotmail.com>
2015-05-28 19:01:10 +0200

    Added Code Implementation for Typical Compound transection

--- src/main/java/um/edu/mt/cps2002/assignment/part1/TransactionManager.java
+++ src/main/java/um/edu/mt/cps2002/assignment/part1/TransactionManager.java
@@ -68,6 +68,34 @@ public class TransactionManager{
     public CompoundTransaction createNewTypicalCompoundTransaction(Risk risk ,int dstPayDeposit, long depositAmount, int[] dstMainTransaction, long[] mainAmount){
         CompoundTransaction root = new CompoundTransaction();
 
+        CompoundTransaction mainTransaction = new CompoundTransaction();
+        CompoundTransaction payCommissionTransaction = new CompoundTransaction();
+
+        int tempSrcAcc = Risk.HIGH == risk ? 3123:8665;
+
+        Transaction payDeposit = new Transaction(this.getAccountDatabase().getAccount(tempSrcAcc),this.getAccountDatabase().getAccount(dstPayDeposit), depositAmount);
+
+        tempSrcAcc = Risk.HIGH == risk? 3143 : 3133;
+        for ( int loops = 0 ; loops < dstMainTransaction.length; loops ++){
+            Transaction tran = new Transaction(this.getAccountDatabase().getAccount(tempSrcAcc),(this.getAccountDatabase().getAccount(dstMainTransaction[loops])), mainAmount[loops]);
+            mainTransaction.addChildTransaction(tran);
+        }
+
+        tempSrcAcc = Risk.HIGH == risk? 6565 : 6588;
+        int tempDscAcc = Risk.HIGH == risk? 4444 : 4445;
+
+        for ( int loops = 0 ; loops < dstMainTransaction.length; loops ++){
+            Transaction tran = new Transaction(this.getAccountDatabase().getAccount(tempSrcAcc)
+                    ,(this.getAccountDatabase().getAccount(tempDscAcc)),
+                    (long)(Risk.HIGH == risk? mainAmount[loops]*0.1 : mainAmount[loops]*0.05));
+
+            payCommissionTransaction.addChildTransaction(tran);
+        }
+
+        root.addChildTransaction(payDeposit);
+        root.addChildTransaction(mainTransaction);
+        root.addChildTransaction(payCommissionTransaction);
+
         return root;
     }
     
