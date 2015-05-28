7bafa840992b8e98cef5091b4590ea3c7c396935
kylebonnici <kylebonnici@hotmail.com>
2015-05-28 19:31:02 +0200

    Implemented Filters and Ascending/ Descending order Extraction Atomic transections from compound transections

--- src/main/java/um/edu/mt/cps2002/assignment/part1/CompoundTransaction.java
+++ src/main/java/um/edu/mt/cps2002/assignment/part1/CompoundTransaction.java
@@ -82,19 +82,49 @@ public class CompoundTransaction extends AbstractTransaction {
     public ArrayList<Transaction> getFilteredTransactions(int srcAcc){
         ArrayList<Transaction> out = new ArrayList<Transaction>();
 
+        for (int loops = 0 ; loops < childTransaction.size(); loops ++){
+            if (childTransaction.get(loops) instanceof Transaction){
+                if ( ((Transaction)childTransaction.get(loops)).getSourceAccountNumber() == srcAcc ){
+                    out.add((Transaction)childTransaction.get(loops) );
+                }
+            }else {
+                out.addAll(((CompoundTransaction)childTransaction.get(loops)).getFilteredTransactions(srcAcc));
+            }
+        }
+
         return out;
     }
 
     public ArrayList<Transaction> getTransactionsInAscending(){
         ArrayList<Transaction> out = new ArrayList<Transaction>();
 
+        for (int loops = 0 ; loops < childTransaction.size(); loops ++){
+            if (childTransaction.get(loops) instanceof Transaction){
+                out.add((Transaction)childTransaction.get(loops));
+            }else {
+                out.addAll(((CompoundTransaction) childTransaction.get(loops)).getTransactionsInAscending());
+            }
+        }
+
+        Collections.sort(out, new Comparator<Transaction>() {
+            @Override
+            public int compare(Transaction z1, Transaction z2) {
+                if (z1.getAmount() > z2.getAmount())
+                    return 1;
+                if (z1.getAmount() < z2.getAmount())
+                    return -1;
+                return 0;
+            }
+        });
 
         return out;
     }
 
     public ArrayList<Transaction> getTransactionsInDescending(){
         ArrayList<Transaction> out = getTransactionsInAscending();
-        
+
+        Collections.reverse(out);
+
         return out;
     }
 }
